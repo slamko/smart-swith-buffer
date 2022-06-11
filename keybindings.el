781f6e08d5eb554c6ef95aab8122d31a933b413e
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-11 17:43:49 +0200

    Switch to next and previous user opened buffer

---
+++ keybindings.el
@@ -0,0 +1,5 @@
+(load-file "smart-buffer-switch.el")
+
+(global-set-key (kbd "M-<tab>") 'switch-to-next-file-buffer)
+(global-set-key (kbd "C-<tab>") 'switch-to-next-user-buffer)
+
--- smart-buffer-switch.el
+++ smart-buffer-switch.el
@@ -9,20 +9,28 @@
 (defmacro setq-ifnil (variable value)
   (setq variable (or variable value)))
 
-(defun do-switch-buffer-file (switch-fun &optional first-buffer)
+(defun do-switch-user-buffer (switch-fun &optional first-buffer)
   (if (not (string= first-buffer (current-buffer-name)))
   (funcall switch-fun)
   (setq-ifnil first-buffer (current-buffer-name)))
   (if (or (string= "*" (substring (current-buffer-name) 0 1))
 		   (string= "%" (substring (current-buffer-name) 0 1)))
-	  (do-switch-buffer-file switch-fun first-buffer))))
-  
+	  (do-switch-buffer-file switch-fun first-buffer)))
+
+(defun switch-to-next-user-buffer ()
+  (interactive)
+  (do-switch-user-buffer 'switch-to-next-buffer))
+
+(defun switch-to-prev-user-buffer ()
+  (interactive)
+  (do-switch-user-buffer 'switch-to-prev-buffer))
+
 (defun do-switch-buffer-file (switch-fun &optional first-buffer)
   (if (not (string= first-buffer (current-buffer-name)))
   (funcall switch-fun)
   (setq-ifnil first-buffer (current-buffer-name)))
   (if (not buffer-file-name)
-	  (do-switch-buffer-file switch-fun first-buffer))))
+	  (do-switch-buffer-file switch-fun first-buffer)))
 
 (defun switch-to-next-file-buffer ()
   (interactive)
@@ -31,5 +39,3 @@
 (defun switch-to-prev-file-buffer ()
   (interactive)
   (do-switch-buffer-file 'switch-to-prev-buffer))
-
- (global-set-key (kbd "M-<tab>") 'switch-to-next-file-buffer)
