3ef3d32f700bcc1c35337078f54c6ce0c34eebae
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-11 17:13:57 +0200

    Stop iteratinf buffer when riched the starting point

--- smart-buffer-switch.el
+++ smart-buffer-switch.el
@@ -1,10 +1,28 @@
 
 ;; Smart buffer switch
 
-(defun do-switch-buffer-file (switch-fun)
+;;
+
+(defun current-buffer-name ()
+  (buffer-name (current-buffer)))
+
+(defmacro setq-ifnil (variable value)
+  (setq variable (or variable value)))
+
+(defun do-switch-buffer-file (switch-fun &optional first-buffer)
+  (if (not (string= first-buffer (current-buffer-name)))
   (funcall switch-fun)
-  (if (string= "*" (substring (buffer-name (current-buffer)) 0 1))
-	  (do-switch-buffer-file)))
+  (setq-ifnil first-buffer (current-buffer-name)))
+  (if (or (string= "*" (substring (current-buffer-name) 0 1))
+		   (string= "%" (substring (current-buffer-name) 0 1)))
+	  (do-switch-buffer-file switch-fun first-buffer))))
+  
+(defun do-switch-buffer-file (switch-fun &optional first-buffer)
+  (if (not (string= first-buffer (current-buffer-name)))
+  (funcall switch-fun)
+  (setq-ifnil first-buffer (current-buffer-name)))
+  (if (not buffer-file-name)
+	  (do-switch-buffer-file switch-fun first-buffer))))
 
 (defun switch-to-next-file-buffer ()
   (interactive)
@@ -14,4 +32,4 @@
   (interactive)
   (do-switch-buffer-file 'switch-to-prev-buffer))
 
-;; (global-set-key (kbd "M-<tab>") 'switch-to-next-file-buffer)
+ (global-set-key (kbd "M-<tab>") 'switch-to-next-file-buffer)
