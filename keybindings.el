7190a570c5578100e9daeaba19522e9a1f653d4e
Viacheslav <vaceslavkozin619@gmail.com>
2022-06-12 13:42:01 +0200

    It is really working

--- keybindings.el
+++ keybindings.el
@@ -2,4 +2,4 @@
 
 (global-set-key (kbd "M-<tab>") 'switch-to-next-file-buffer)
 (global-set-key (kbd "C-<tab>") 'switch-to-next-user-buffer)
-
+(global-set-key (kbd "C-/") 'switch-to-next-dir-buffer)
--- smart-buffer-switch.el
+++ smart-buffer-switch.el
@@ -6,16 +6,21 @@
 (defun current-buffer-name ()
   (buffer-name (current-buffer)))
 
-(defmacro setq-ifnil (variable value)
+(defun setq-ifnil (variable value)
   (setq variable (or variable value)))
+
+(defun sys-buffer-p ()
+  (string= "*" (substring (current-buffer-name) 0 1)))
+
+;; switch to user buffer
 
 (defun do-switch-user-buffer (switch-fun &optional first-buffer)
   (if (not (string= first-buffer (current-buffer-name)))
-  (funcall switch-fun)
-  (setq-ifnil first-buffer (current-buffer-name)))
-  (if (or (string= "*" (substring (current-buffer-name) 0 1))
-		   (string= "%" (substring (current-buffer-name) 0 1)))
-	  (do-switch-buffer-file switch-fun first-buffer)))
+	  (progn
+		(setq first-buffer (or first-buffer (current-buffer-name)))
+		(funcall switch-fun)
+		(if (sys-buffer-p)
+			(do-switch-user-buffer switch-fun first-buffer)))))
 
 (defun switch-to-next-user-buffer ()
   (interactive)
@@ -25,12 +30,34 @@
   (interactive)
   (do-switch-user-buffer 'switch-to-prev-buffer))
 
+;; switch to directory buffer
+
+(defun do-switch-directory-buffer (switch-fun &optional first-buffer)
+   (if (not (string= first-buffer (current-buffer-name)))
+   	(progn
+   	  (setq first-buffer (or first-buffer (current-buffer-name)))
+   	  (funcall switch-fun)
+   	  (if (or (sys-buffer-p) buffer-file-name)
+   		  (do-switch-directory-buffer switch-fun first-buffer)))))
+
+(defun switch-to-next-dir-buffer ()
+  (interactive)
+  (do-switch-directory-buffer 'switch-to-next-buffer))
+
+(defun switch-to-prev-dir-buffer ()
+  (interactive)
+  (do-switch-directory-buffer 'switch-to-prev-buffer))
+
+
+;; switch to file buffer
+
 (defun do-switch-buffer-file (switch-fun &optional first-buffer)
   (if (not (string= first-buffer (current-buffer-name)))
-  (funcall switch-fun)
-  (setq-ifnil first-buffer (current-buffer-name)))
-  (if (not buffer-file-name)
-	  (do-switch-buffer-file switch-fun first-buffer)))
+	  (progn
+		(setq-ifnil first-buffer (current-buffer-name))
+		(funcall switch-fun)
+		(if (not buffer-file-name)
+			(do-switch-buffer-file switch-fun first-buffer)))))
 
 (defun switch-to-next-file-buffer ()
   (interactive)
